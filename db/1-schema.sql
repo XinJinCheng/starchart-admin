fc1c9a353685323c917148c6cf9bb0285f78ed27
Gary Zhu <dinner3000@hotmail.com>
2018-09-25 16:42:18 +0800

    feat: tuning deploy shell script

--- db/1-schema.sql
+++ db/1-schema.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `starchart` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE  IF NOT EXISTS `starchart` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
 USE `starchart`;
 
 -- 菜单
